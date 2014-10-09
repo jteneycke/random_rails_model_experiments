868739b59520e505e2e795e1db28a39b934cd4fb
Josh Teneycke <josh.teneycke@gmail.com>
2014-10-09 18:05:31 -0400

    Make line nice.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,5 @@
 class Client < ActiveRecord::Base
-  has_many :subordinates, class_name: "Client",
-                          foreign_key: "manager_id"
+  has_many :subordinates, class_name: "Client", foreign_key: "manager_id"
   belongs_to :manager, class_name: "Client"
 
   has_many :orders
